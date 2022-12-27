36bffa7b97958a53f282ae85c94e0a76dbb26ad6
Kiwifuit <mahkiwi123@gmail.com>
2022-12-27 22:03:25 +0800

    Removed `items` variable, opting to inline the whole thing

--- src/quiz.rs
+++ src/quiz.rs
@@ -9,12 +9,10 @@ pub struct Quiz {
 
 impl Quiz {
     pub fn new(name: &str, perfect_score: u8, items: Vec<(&str, &str)>) -> Self {
-        let items = items.iter().map(|&i| QuizItem::new(i.0, i.1)).collect();
-
         Self {
             name: name.to_string(),
             perfect_score,
-            items
+            items: items.iter().map(|&i| QuizItem::new(i.0, i.1)).collect()
         }
     }
 }
