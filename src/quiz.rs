9203c797c274867593581e0bc69602714798fd37
Kiwifuit <mahkiwi123@gmail.com>
2022-12-27 20:53:11 +0800

    Added method to create new `Quiz` struct, removed publicity of `items`

--- src/quiz.rs
+++ src/quiz.rs
@@ -2,7 +2,21 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Quiz {
-    pub items: Vec<QuizItem>
+    name: String,
+    perfect_score: u8,
+    items: Vec<QuizItem>
+}
+
+impl Quiz {
+    pub fn new(name: String, perfect_score: u8, items: Vec<(&str, &str)>) -> Self {
+        let items = items.iter().map(|&i| QuizItem::new(i.0, i.1)).collect();
+
+        Self {
+            name,
+            perfect_score,
+            items
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,10 +26,10 @@ pub struct QuizItem {
 }
 
 impl QuizItem {
-    pub fn new(prompt: String, answer: String) -> Self {
+    pub fn new(prompt: &str, answer: &str) -> Self {
         Self {
-            prompt,
-            answer
+            prompt: prompt.to_string(),
+            answer: answer.to_string()
         }
     }
 }
