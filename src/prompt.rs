c219dc2b8d10676c70d2a6a22fea03400f6497c8
Kiwifuit <mahkiwi123@gmail.com>
2022-12-28 14:29:56 +0800

    Gave `prompt::ErrorKind::ParseError` a `String`

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,47 +1,56 @@
 use std::fmt::Display;
-use std::io::{Write, stdout, stdin};
+use std::io::{stdin, stdout, Write};
 use std::str::FromStr;
 
 #[derive(Debug)]
 pub enum ErrorKind {
     StdoutFlushError(String),
     StdinReadError(String),
-    ParseError,
-    ValidationError
+    ParseError(String),
+    ValidationError,
 }
 
 impl Display for ErrorKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            Self::ParseError => String::from("Unable to parse data"),
-            Self::ValidationError => String::from("Unable to validate data"),
-            Self::StdoutFlushError(e) => format!("Unable to flush stdout: {}", e),
-            Self::StdinReadError(e) => format!("Unable to read stdin: {}", e)
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::ValidationError => String::from("Unable to validate data"),
+                Self::ParseError(e) => format!("Unable to parse data: {}", e),
+                Self::StdoutFlushError(e) => format!("Unable to flush stdout: {}", e),
+                Self::StdinReadError(e) => format!("Unable to read stdin: {}", e),
+            }
+        )
     }
 }
 
-pub fn prompt<Return, Prompt>(prompt:Prompt, validation: fn(&String) -> bool) -> Result<Return, ErrorKind>
-    where
+pub fn prompt<Return, Prompt>(
+    prompt: Prompt,
+    validation: fn(&String) -> bool,
+) -> Result<Return, ErrorKind>
+where
     Prompt: Display,
-    Return: FromStr {
+    Return: FromStr,
+    <Return as FromStr>::Err: ToString,
+{
     let mut buffer = String::new();
 
     print!("{}: ", prompt);
     match stdout().flush() {
         Ok(_) => (),
-        Err(e) => return Err(ErrorKind::StdoutFlushError(e.to_string()))
+        Err(e) => return Err(ErrorKind::StdoutFlushError(e.to_string())),
     };
 
     match stdin().read_line(&mut buffer) {
         Ok(_) => (),
-        Err(e) => return Err(ErrorKind::StdinReadError(e.to_string()))
+        Err(e) => return Err(ErrorKind::StdinReadError(e.to_string())),
     };
 
     if validation(&buffer) {
         return match buffer.parse() {
             Ok(parsed) => Ok(parsed),
-            Err(_) => Err(ErrorKind::ParseError)
+            Err(e) => Err(ErrorKind::ParseError(e.to_string())),
         };
     }
     Err(ErrorKind::ValidationError)
