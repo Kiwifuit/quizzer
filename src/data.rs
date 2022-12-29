5f372c5ca8b483f9adaae3a8b2b1c1addaed99bc
Kiwifuit <mahkiwi123@gmail.com>
2022-12-29 08:36:52 +0800

    Generalized `read_quiz` and `write_quiz` for testing

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,8 @@
-use std::{fs::File, io::{Write, Read}, fmt::Display};
+use std::fmt::Display;
+use std::io::{Read, Write};
 
-use bincode::{serialize, deserialize};
 use crate::quiz::Quiz;
+use bincode::{deserialize, serialize};
 
 #[derive(Debug)]
 pub enum ErrorKind {
@@ -13,38 +14,48 @@ pub enum ErrorKind {
 
 impl Display for ErrorKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            Self::DeserializationError(e) => format!("Unable to read data: {}", e),
-            Self::SerializationError(e) => format!("Unable to write data: {}", e),
-            Self::ReadError(e) => format!("Unable to read file: {}", e),
-            Self::WriteError(e) => format!("Unable to write data to file: {}", e),
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::DeserializationError(e) => format!("Unable to read data: {}", e),
+                Self::SerializationError(e) => format!("Unable to write data: {}", e),
+                Self::ReadError(e) => format!("Unable to read file: {}", e),
+                Self::WriteError(e) => format!("Unable to write data to file: {}", e),
+            }
+        )
     }
 }
 
-pub fn save_quiz(quiz: &Quiz, file: &mut File) -> Result<usize, ErrorKind>{
+pub fn save_quiz<F>(quiz: &Quiz, file: &mut F) -> Result<usize, ErrorKind>
+where
+    F: Write,
+{
     let data = match serialize(quiz) {
         Ok(d) => d,
-        Err(e) => return Err(ErrorKind::SerializationError(e.to_string()))
+        Err(e) => return Err(ErrorKind::SerializationError(e.to_string())),
     };
 
     match file.write(&data) {
         Ok(size) => Ok(size),
-        Err(e) => Err(ErrorKind::WriteError(e.to_string()))
+        Err(e) => Err(ErrorKind::WriteError(e.to_string())),
     }
 }
 
-pub fn read_quiz(file: &mut File) -> Result<Quiz, ErrorKind> {
+pub fn read_quiz<F>(file: &mut F) -> Result<Quiz, ErrorKind>
+where
+    F: Read,
+{
     let mut raw = vec![];
 
     match file.read(&mut raw) {
         Ok(_) => (),
-        Err(e) => return Err(ErrorKind::ReadError(e.to_string()))
+        Err(e) => return Err(ErrorKind::ReadError(e.to_string())),
     };
 
     let quiz = match deserialize(&raw) {
         Ok(q) => q,
-        Err(e) => return Err(ErrorKind::DeserializationError(e.to_string()))
+        Err(e) => return Err(ErrorKind::DeserializationError(e.to_string())),
     };
 
     Ok(quiz)
